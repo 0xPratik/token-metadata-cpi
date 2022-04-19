d6e43a0ace3d179b5c48e101cb9f00be130c9115
ProPratik1405 <sariapratik@gmail.com>
2022-04-19 18:07:32 +0530

    Update metadata added

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use anchor_lang::{solana_program, Result};
 use mpl_token_metadata::instruction;
 use mpl_token_metadata::state::Collection;
 use mpl_token_metadata::state::Creator;
+use mpl_token_metadata::state::Data;
 use mpl_token_metadata::state::Uses;
 pub use mpl_token_metadata::ID;
 // use std::ops::Deref;
@@ -58,6 +59,88 @@ pub struct CreateMetadataAccountV2<'info> {
     pub mint_authority: AccountInfo<'info>,
     pub payer: AccountInfo<'info>,
     pub update_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+pub fn create_metadata_accounts<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, CreateMetadataAccounts<'info>>,
+    name: String,
+    symbol: String,
+    uri: String,
+    seller_fee_basis_points: u16,
+    update_authority_is_signer: bool,
+    is_mutable: bool,
+    creators: Vec<Creator>,
+) -> Result<()> {
+    let ix = instruction::create_metadata_accounts(
+        mpl_token_metadata::ID,
+        ctx.accounts.metadata_account.key(),
+        ctx.accounts.mint.key(),
+        ctx.accounts.mint_authority.key(),
+        ctx.accounts.payer.key(),
+        ctx.accounts.update_authority.key(),
+        name,
+        symbol,
+        uri,
+        Some(creators),
+        seller_fee_basis_points,
+        update_authority_is_signer,
+        is_mutable,
+    );
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.metadata_account,
+            ctx.accounts.mint,
+            ctx.accounts.mint_authority,
+            ctx.accounts.payer,
+            ctx.accounts.update_authority,
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+        ctx.signer_seeds,
+    )
+    .map_err(Into::into)
+}
+
+#[derive(Accounts)]
+pub struct CreateMetadataAccounts<'info> {
+    pub metadata_account: AccountInfo<'info>,
+    pub mint: AccountInfo<'info>,
+    pub mint_authority: AccountInfo<'info>,
+    pub payer: AccountInfo<'info>,
+    pub update_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+pub fn update_metadata_accounts<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, UpdateMetadataAccounts<'info>>,
+    primary_sale_happened: bool,
+    data: Data,
+) -> Result<()> {
+    let ix = instruction::update_metadata_accounts(
+        mpl_token_metadata::ID,
+        ctx.accounts.metadata_account.key(),
+        ctx.accounts.update_authority.key(),
+        Some(ctx.accounts.new_update_authority.key()),
+        Some(data),
+        Some(primary_sale_happened),
+    );
+    solana_program::program::invoke_signed(
+        &ix,
+        &[ctx.accounts.metadata_account, ctx.accounts.update_authority],
+        ctx.signer_seeds,
+    )
+    .map_err(Into::into)
+}
+
+#[derive(Accounts)]
+pub struct UpdateMetadataAccounts<'info> {
+    pub metadata_account: AccountInfo<'info>,
+    pub update_authority: AccountInfo<'info>,
+    pub new_update_authority: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 }
